c9e1d399ea6fd6ac41b28f1a11bdb65a218a3070
k2works <kakimomokuri@gmail.com>
2016-09-03 10:58:41 +0900

    JQueryサンプル
    [フォーム]テキストやテキストエリアのリアルタイム入力チェック

--- app/assets/javascripts/jquery_sample_form.js
+++ app/assets/javascripts/jquery_sample_form.js
@@ -1,129 +1,298 @@
 $(document).on("turbolinks:load", function() {
-    function setMyForm(target){
+    $(function () {
+        function setMyForm(target){
 
-        var ERROR_MESSAGE_CLASSNAME = 'errorMsg';
-        var ERROR_INPUT_CLASSNAME = 'errorInput';
+            var ERROR_MESSAGE_CLASSNAME = 'errorMsg';
+            var ERROR_INPUT_CLASSNAME = 'errorInput';
 
-        var items = [];
+            var items = [];
 
-        var checkAll = function(){
-            var errorCount = 0;
-            checkEmptyText( items[0], '名前をご入力ください。');
-            if( items[0].prop('isSuccess')) checkFormatText( items[0], 0, '入力フォーマットが正しくありません。');
-            checkEmptyText( items[1], 'ふりがなをご入力ください。');
-            if( items[1].prop('isSuccess')) checkFormatText( items[1], 0, '入力フォーマットが正しくありません。');
-            checkEmptyText( items[2], '電話番号をご入力ください。');
-            if( items[2].prop('isSuccess')) checkFormatText( items[2], 0, '入力フォーマットが正しくありません。');
-            checkEmptyText( items[3], 'メールアドレスをご入力ください。');
-            if( items[3].prop('isSuccess')) checkFormatText( items[3], 0, '入力フォーマットが正しくありません。');
-            checkEmptyText( items[4], 'お問い合わせ内容をご入力ください。');
-            for( var i=0; i<items.length; i++){
-                if( items[i].prop('isSuccess') == false ){
-                    errorCount++;
+            var checkAll = function(){
+                var errorCount = 0;
+                checkEmptyText( items[0], '名前をご入力ください。');
+                if( items[0].prop('isSuccess')) checkFormatText( items[0], 0, '入力フォーマットが正しくありません。');
+                checkEmptyText( items[1], 'ふりがなをご入力ください。');
+                if( items[1].prop('isSuccess')) checkFormatText( items[1], 0, '入力フォーマットが正しくありません。');
+                checkEmptyText( items[2], '電話番号をご入力ください。');
+                if( items[2].prop('isSuccess')) checkFormatText( items[2], 0, '入力フォーマットが正しくありません。');
+                checkEmptyText( items[3], 'メールアドレスをご入力ください。');
+                if( items[3].prop('isSuccess')) checkFormatText( items[3], 0, '入力フォーマットが正しくありません。');
+                checkEmptyText( items[4], 'お問い合わせ内容をご入力ください。');
+                for( var i=0; i<items.length; i++){
+                    if( items[i].prop('isSuccess') == false ){
+                        errorCount++;
+                    }
+                }
+                if( errorCount == 0){
+                    alert('送信内容にエラーはありません。');
+                }
+            };
+
+            function checkFormatText(selector, mode, msg){
+                var value = selector.val();
+                switch(mode){
+                    //全角のみ
+                    case 0:
+                        if(value.match(/^[^ -~｡-ﾟ]*$/)) {
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                    //ふりがなのみ
+                    case 1:
+                        if(value.match(/^[\u3040-\u309F]+$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                    //半角数字のみ
+                    case 2:
+                        if(value.match(/^[0-9]*$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                    //メールアドレスかどうか
+                    case 3:
+                        if(value.match(/^[a-zA-Z0-9!$&*.=^'|~#%'+\/?_{}-]+@([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6}$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                }
+                if(selector.prop('isSuccess') == false ){
+                    addErrorMessage(selector, msg);
+                }else{
+                    removeErrorMessage(selector);
                 }
             }
-            if( errorCount == 0){
-                alert('送信内容にエラーはありません。');
-            }
-        };
 
-        function checkFormatText(selector, mode, msg){
-            var value = selector.val();
-            switch(mode){
-                //全角のみ
-                case 0:
-                    if(value.match(/^[^ -~｡-ﾟ]*$/)) {
-                        selector.prop('isSuccess', true);
-                    }else{
-                        selector.prop('isSuccess', false);
-                    }
-                    break;
-                //ふりがなのみ
-                case 1:
-                    if(value.match(/^[\u3040-\u309F]+$/)){
-                        selector.prop('isSuccess', true);
-                    }else{
-                        selector.prop('isSuccess', false);
-                    }
-                    break;
-                //半角数字のみ
-                case 2:
-                    if(value.match(/^[0-9]*$/)){
-                        selector.prop('isSuccess', true);
-                    }else{
-                        selector.prop('isSuccess', false);
-                    }
-                    break;
-                //メールアドレスかどうか
-                case 3:
-                    if(value.match(/^[a-zA-Z0-9!$&*.=^'|~#%'+\/?_{}-]+@([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6}$/)){
-                        selector.prop('isSuccess', true);
-                    }else{
-                        selector.prop('isSuccess', false);
-                    }
-                    break;
-            }
-            if(selector.prop('isSuccess') == false ){
-                addErrorMessage(selector, msg);
-            }else{
+            var addErrorMessage = function(selector, msg){
                 removeErrorMessage(selector);
-            }
+                selector.before('<span class="'+ERROR_MESSAGE_CLASSNAME+'">'+msg+'</span>');
+                selector.addClass(ERROR_INPUT_CLASSNAME);
+            };
+
+            var removeErrorMessage = function(selector){
+                var msgSelector = selector.parent().find('.'+ERROR_MESSAGE_CLASSNAME);
+                if( msgSelector.length != 0){
+                    msgSelector.remove();
+                    selector.removeClass(ERROR_INPUT_CLASSNAME);
+                }
+            };
+
+            var checkEmptyText = function (selector, msg) {
+                if( selector.val() == ''){
+                    addErrorMessage(selector, msg);
+                    selector.prop('isSuccess', false);
+                }else{
+                    removeErrorMessage(selector);
+                    selector.prop('isSuccess', true);
+                }
+            };
+
+            var init = function(){
+                target.on({
+                    'submit': function () {
+                        checkAll();
+                        return false;
+                    }
+                });
+
+                items = [
+                    target.find('input[name=formName]'),
+                    target.find('input[name=formFurigana]'),
+                    target.find('input[name=formTell]'),
+                    target.find('input[name=formMail]'),
+                    target.find('textarea[name=formInquiry]')
+                ];
+
+                $.each(items, function(index){
+                    items[index].prop('isSuccess', false);
+                });
+                target.find('input[type=text]').on({
+                    'keypress': function (e) {
+                        if( (e.keyCode == 13)) return false;
+                    }
+                });
+            };
+
+            init();
+
         }
 
-        var addErrorMessage = function(selector, msg){
-            removeErrorMessage(selector);
-            selector.before('<span class="'+ERROR_MESSAGE_CLASSNAME+'">'+msg+'</span>');
-            selector.addClass(ERROR_INPUT_CLASSNAME);
-        };
+        setMyForm($('#myForm'));
+    });
 
-        var removeErrorMessage = function(selector){
-            var msgSelector = selector.parent().find('.'+ERROR_MESSAGE_CLASSNAME);
-            if( msgSelector.length != 0){
-                msgSelector.remove();
-                selector.removeClass(ERROR_INPUT_CLASSNAME);
+    $(function(){
+
+        function setMyForm2(target){
+
+            var ERROR_MESSAGE_CLASSNAME = 'errorMsg'; //エラー時のメッセージ要素のclass名
+            var ERROR_INPUT_CLASSNAME = 'errorInput'; //エラー時のinput要素のclass名
+
+            var items = []; //チェック対象となるテキスト入力要素を格納した配列
+
+            //項目チェックする
+            function checkAll(){
+                var errorCount = 0;
+                //input,textareaのチェック
+                for( var i=0; i<items.length; i++ ){
+                    items[i].blur();
+                    if( items[i].prop('isSuccess') == false ){
+                        errorCount++;
+                    }
+                }
+                //エラーカウントが0であれば、エラー無し
+                if( errorCount == 0 ){
+                    alert( '送信内容にエラーはありません。' );
+                }
             }
-        };
 
-        var checkEmptyText = function (selector, msg) {
-            if( selector.val() == ''){
-                addErrorMessage(selector, msg);
-                selector.prop('isSuccess', false);
-            }else{
+            //エラーメッセージの追加
+            function addErrorMessage(selector, msg){
                 removeErrorMessage(selector);
-                selector.prop('isSuccess', true);
+                selector.before('<span class="'+ERROR_MESSAGE_CLASSNAME+'">'+msg+'</span>');
+                selector.addClass(ERROR_INPUT_CLASSNAME);
             }
-        };
 
-        var init = function(){
-            target.on({
-                'submit': function () {
-                    checkAll();
-                    return false;
+            //エラーメッセージの削除
+            function removeErrorMessage(selector){
+                var msgSelector = selector.parent().find('.'+ERROR_MESSAGE_CLASSNAME);
+                if( msgSelector.length != 0 ){
+                    msgSelector.remove();
+                    selector.removeClass(ERROR_INPUT_CLASSNAME);
                 }
-            });
+            }
 
-            items = [
-                target.find('input[name=formName]'),
-                target.find('input[name=formFurigana]'),
-                target.find('input[name=formTell]'),
-                target.find('input[name=formMail]'),
-                target.find('textarea[name=formInquiry]')
-            ];
-
-            $.each(items, function(index){
-                items[index].prop('isSuccess', false);
-            });
-            target.find('input[type=text]').on({
-                'keypress': function (e) {
-                    if( (e.keyCode == 13)) return false;
+            //input,textareaの未入力チェック
+            function checkEmptyText(selector, msg){
+                if( selector.val() == '' ){
+                    addErrorMessage(selector, msg);
+                    selector.prop('isSuccess', false);
+                }else{
+                    removeErrorMessage(selector);
+                    selector.prop('isSuccess', true);
                 }
-            });
-        };
+            }
 
-        init();
+            //inputのフォーマットチェック
+            function checkFormatText(selector, mode, msg){
+                var value = selector.val();
+                switch(mode){
+                    //全角チェック
+                    case 0:
+                        if(value.match(/^[^ -~｡-ﾟ]*$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                    //ふりがなのみ
+                    case 1:
+                        if(value.match(/^[\u3040-\u309F]+$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                    //半角数字のみ
+                    case 2:
+                        if(value.match(/^[0-9]*$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                    //メールアドレス
+                    case 3:
+                        if(value.match(/^[a-zA-Z0-9!$&*.=^`|~#%'+\/?_{}-]+@([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6}$/)){
+                            selector.prop('isSuccess', true);
+                        }else{
+                            selector.prop('isSuccess', false);
+                        }
+                        break;
+                }
+                if( selector.prop('isSuccess') == false ){
+                    addErrorMessage(selector, msg);
+                }else{
+                    removeErrorMessage(selector);
+                }
+            }
 
-    }
+            //初期設定
+            function init(){
+                //submitイベントの設定
+                target.on({
+                    'submit': function(){
+                        checkAll();
+                        return false;
+                    }
+                });
+                //input要素を配列に格納
+                items = [
+                    target.find('input[name=formName2]'),
+                    target.find('input[name=formFurigana2]'),
+                    target.find('input[name=formTell2]'),
+                    target.find('input[name=formMail2]'),
+                    target.find('textarea[name=formInquiry2]')
+                ];
+                //input要素のプロパティを設定
+                $.each(items, function(index){
+                    items[index].prop('isSuccess', false);
+                });
+                //enterキーでsubmitしてしまうのを防止する
+                target.find('input[type=text]').on({
+                    'keypress': function(e){
+                        if( (e.keyCode == 13) ) return false;
+                    }
+                });
+                //項目：名前のセレクタにblurイベントを設定
+                items[0].on({
+                    'blur': function(){
+                        checkEmptyText( $(this), '名前をご入力ください。' );
+                        if( $(this).prop('isSuccess') ) checkFormatText( $(this), 0, '入力フォーマットが正しくありません。' );
+                    }
+                });
+                //項目：ふりがなのセレクタにblurイベントを設定
+                items[1].on({
+                    'blur': function(){
+                        checkEmptyText( $(this), 'ふりがなをご入力ください。' );
+                        if( $(this).prop('isSuccess') ) checkFormatText( $(this), 1, '入力フォーマットが正しくありません。' );
+                    }
+                });
+                //項目：電話番号のセレクタにblurイベントを設定
+                items[2].on({
+                    'blur': function(){
+                        checkEmptyText( $(this), '電話番号をご入力ください。' );
+                        if( $(this).prop('isSuccess') ) checkFormatText( $(this), 2, '入力フォーマットが正しくありません。' );
+                    }
+                });
+                //項目：メールアドレスのセレクタにblurイベントを設定
+                items[3].on({
+                    'blur': function(){
+                        checkEmptyText( $(this), 'メールアドレスをご入力ください。' );
+                        if( $(this).prop('isSuccess') ) checkFormatText( $(this), 3, '入力フォーマットが正しくありません。' );
+                    }
+                });
+                //項目：お問い合わせ内容のセレクタにblurイベントを設定
+                items[4].on({
+                    'blur': function(){
+                        checkEmptyText( $(this), 'お問い合わせ内容をご入力ください。' );
+                    }
+                });
+            }
 
-    setMyForm($('#myForm'));
+            init();
+
+        }
+
+        setMyForm2($('#myForm2'));
+
+    });
 
 });
