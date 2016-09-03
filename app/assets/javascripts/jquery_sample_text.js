4b8207100d0062f4226361e9ced8e558106718ff
k2works <kakimomokuri@gmail.com>
2016-09-03 16:38:49 +0900

    JQueryサンプル
    [テキスト]シャッフル表示

--- app/assets/javascripts/jquery_sample_text.js
+++ app/assets/javascripts/jquery_sample_text.js
@@ -151,4 +151,43 @@ $(document).on("turbolinks:load", function() {
         });
     });
 
+    $(function () {
+
+        var SHUFFLE_STEP = 10; //シャッフルの間隔値
+        var SHUFFLE_STR = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわん'; //シャッフル文字列
+        var COUNT_STEP = 80; //カウントダウンの間隔値
+
+        function setShuffleAnimate(target) {
+
+            var str = target.text();
+            var count = -1;
+            var txt = '';
+
+            function start(){
+                count = -1;
+                var shuffleID = setInterval(
+                    function () {
+                        target.text('');
+                        for( var i=0; i<str.length; i++){
+                            txt = SHUFFLE_STR[ Math.floor(Math.random()*SHUFFLE_STR.length) ];
+                            if ( count >= i ) txt = str.charAt(i);
+                            target.append(txt);
+                        }
+                        if( count >= str.length ){
+                            clearInterval(shuffleID);
+                            clearInterval(countID);
+                        }
+                    },
+                SHUFFLE_STEP);
+                var countID = setInterval(function(){count++;}, COUNT_STEP);
+            }
+            target.on('mouseover', start);
+            start();
+        }
+
+        $.each( $('.shuffle'), function () {
+            setShuffleAnimate($(this));
+        });
+    });
+
 });
