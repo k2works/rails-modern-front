39a0c9d597fd88c7bf6f20c8ef1562c2e0e3c5f2
k2works <kakimomokuri@gmail.com>
2016-09-03 16:19:51 +0900

    JQueryサンプル
    [テキスト]タイピング風に１文字ずつ表示

--- app/assets/javascripts/jquery_sample_text.js
+++ app/assets/javascripts/jquery_sample_text.js
@@ -119,4 +119,36 @@ $(document).on("turbolinks:load", function() {
 
     });
 
+    $(function () {
+
+        var STEP = 100;
+        var TYPING_LINE = '<span class="line">_</span>';
+
+        function setTextAnimate(target){
+            var str = target.text();
+            var num = 0;
+            var typingLine;
+
+            target.text('').append(TYPING_LINE);
+            typingLine = target.find('.line');
+
+            for( var i=0; i<str.length; i++){
+                setTimeout(function () {
+                    typingLine.remove();
+                    target.append( str.charAt(num) ).append(typingLine);
+                    num++;
+                }, i*STEP);
+            }
+
+            setTimeout( function () {
+                typingLine.remove();
+            }, str.length*STEP);
+
+        }
+
+        $.each( $('.animate_txt'), function () {
+            setTextAnimate($(this));
+        });
+    });
+
 });
