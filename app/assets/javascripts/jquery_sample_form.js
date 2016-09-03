b54d4bf0b6fdffc6f1c10cb884a0fa23170a8e7d
k2works <kakimomokuri@gmail.com>
2016-09-03 14:28:07 +0900

    JQueryサンプル
    [フォーム]プレースホルダ

--- app/assets/javascripts/jquery_sample_form.js
+++ app/assets/javascripts/jquery_sample_form.js
@@ -910,4 +910,68 @@ $(document).on("turbolinks:load", function() {
 
     });
 
+    $(function () {
+        function setPlaceHolder(className) {
+
+            var target;
+
+            function create(){
+                target.wrap('<span class="input-wrap"></span>');
+                target.after('<span class="placeholder-text">'+target.attr('data-placeholder')+'</span>');
+            }
+
+            function setStyle() {
+                target.parent().css({
+                    'display': 'inline-block',
+                    'position': 'relative',
+                    'width': target.width()+'px',
+                    'height': target.height()+'px'
+                });
+                target.css({
+                    'position': 'absolute',
+                    'top': 0
+                });
+                target.parent().find('.placeholder-text').css({
+                    'position': 'absolute',
+                    'top': 0,
+                    'left': 4+'px',
+                    'width': 250+'px',
+                    'color': '#aaa',
+                    'font-size': 12+'px',
+                    'line-height': 24+'px'
+                });
+            }
+
+            function setEvent(selector) {
+                selector.on({
+                    'focus': function () {
+                        selector.parent().find('.placeholder-text').hide();
+                    },
+                    'blur': function () {
+                        if( selector.val() == '' ) selector.parent().find('.placeholder-text').show();
+                    }
+                });
+                selector.parent().find('.placeholder-text').on({
+                    'click': function () {
+                        selector.focus();
+                    }
+                });
+                if( selector.val().length != '' ) selector.focus();
+            }
+
+            function init(){
+                $.each($('.'+className), function () {
+                    target = $(this);
+                    create();
+                    setStyle();
+                    setEvent(target);
+                });
+            }
+
+            init();
+        }
+
+        setPlaceHolder('placeholder');
+    });
+
 });
